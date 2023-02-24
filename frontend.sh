e2acc44f1055dbd4b8bf6d91d0e3be4037b9a733
anand nandhu <anandnandhu52@gmail.com>
2023-02-24 23:07:16 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -9,7 +9,7 @@ echo -e "/e[35m download frontend content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
 cd /usr/share/nginx/html
-echo -e "\e[35m extract frontend content\e[om"
+echo -e "\e[35m extract frontend content\e[0m"
 unzip /tmp/frontend.zip
 
 echo -e "\e[35m copy roboshop nginx config  file\e[0m"
