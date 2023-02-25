a15bd83d4f1e188a4695ba4ea31c5475f721fc8f
anand nandhu <anandnandhu52@gmail.com>
2023-02-25 14:01:28 +0530

    workstation

--- catalogue.sh
+++ catalogue.sh
@@ -15,6 +15,8 @@ cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.serv
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
+
 cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 yum install mongodb-org-shell -y
+
 mongo --host mongodb-dev.anandnandhu.online </app/schema/catalogue.js
