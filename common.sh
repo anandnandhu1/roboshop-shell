43260e1ebca155f742809dd89aa87653c68b5ec0
anand nandhu <anandnandhu52@gmail.com>
2023-02-28 15:18:25 +0530

    cart

--- common.sh
+++ common.sh
@@ -14,6 +14,50 @@ print_head() {
   echo -e "\e[1m $1 \e[0m"
 }
 
+APP_PREREQ() {
+
+  print_head "add application user"
+    id roboshop &>>${LOG}
+    if [ $? -ne 0 ]; then
+     useradd roboshop &>>${LOG}
+    fi
+    status_check
+
+    mkdir -p /app &>>${LOG}
+
+    print_head "downloading app content"
+    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${LOG}
+    status_check
+
+    print_head "cleanup old content"
+    rm -rf /app/* &>>${LOG}
+    status_check
+
+    print_head "extract app content"
+    cd /app
+    unzip /tmp/${component}.zip &>>${LOG}
+    status_check
+
+}
+
+SYSTEMD_SETUP() {
+    print_head "Configuring ${component} Service File"
+    cp ${script_location}/files/${component}.service /etc/systemd/system/${component}.service &>>${LOG}
+    status_check
+
+    print_head "Reload SystemD"
+    systemctl daemon-reload &>>${LOG}
+    status_check
+
+    print_head "Enable ${component} Service "
+    systemctl enable ${component} &>>${LOG}
+    status_check
+
+    print_head "Start ${component} service "
+    systemctl start ${component} &>>${LOG}
+    status_check
+}
+
 NODEJS() {
   print_head "configure node js repos"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
@@ -23,48 +67,15 @@ NODEJS() {
   yum install nodejs -y &>>${LOG}
   status_check
 
-  print_head "add application user"
-  id roboshop &>>${LOG}
-  if [ $? -ne 0 ]; then
-   useradd roboshop &>>${LOG}
-  fi
-  status_check
-
-  mkdir -p /app &>>${LOG}
-
-  print_head "downloading app content"
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${LOG}
-  status_check
-
-  print_head "cleanup old content"
-  rm -rf /app/* &>>${LOG}
-  status_check
-
-  print_head "extract app content"
-  cd /app
-  unzip /tmp/${component}.zip &>>${LOG}
-  status_check
+  APP_PREREQ
 
   print_head "install nodejs dependancies"
   cd /app &>>${LOG}
   npm install &>>${LOG}
   status_check
 
-  print_head "configure ${component} service file"
-  cp ${script_location}/files/${component}.service /etc/systemd/system/${component}.service &>>${LOG}
-  status_check
+  SYSTEMD_SETUP
 
-  print_head "reload systemd"
-  systemctl daemon-reload &>>${LOG}
-  status_check
-
-  print_head "enable ${component} server"
-  systemctl enable ${component} &>>${LOG}
-  status_check
-
-  print_head "start ${component} server"
-  systemctl start ${component} &>>${LOG}
-  status_check
   if [ ${schema_load} == "true" ]; then
   print_head "configuring mongo repo"
   cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
@@ -78,4 +89,20 @@ NODEJS() {
   mongo --host mongodb-dev.anandnandhu.online </app/schema/${component}.js &>>${LOG}
   status_check
   fi
+}
+
+maven() {
+  print_head "install maven"
+  yum install nodejs -y &>>${LOG}
+  status_check
+
+  APP_PREREQ
+
+  print_head "build a package"
+  mvn clean package &>>${LOG}
+  status_check
+
+  print_head "copy a file to app location"
+  mv target/${component}-1.0.jar ${component}.jar &>>${LOG}
+  status_check
 }
