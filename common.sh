d6b057fb85f30804ce624c1694bab7f297aa2c0b
anand nandhu <anandnandhu52@gmail.com>
2023-02-28 12:06:02 +0530

    redis

--- common.sh
+++ common.sh
@@ -13,3 +13,68 @@ status_check() {
 print_head() {
   echo -e "\e[1m $1 \e[0m"
 }
+
+NODEJS() {
+  print_head "configure node js repos"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+  status_check
+
+  print_head "install nodejs"
+  yum install nodejs -y &>>${LOG}
+  status_check
+
+  print_head "add application user"
+  id roboshop &>>${LOG}
+  if [ $? -ne 0 ]; then
+   useradd roboshop &>>${LOG}
+  fi
+  status_check
+
+  mkdir -p /app &>>${LOG}
+
+  print_head "downloading app content"
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component} &>>${LOG}
+  status_check
+
+  print_head "cleanup old content"
+  rm -rf /app/* &>>${LOG}
+  status_check
+
+  print_head "extract app content"
+  cd /app &>>${LOG}
+  unzip /tmp/${component}.zip &>>${LOG}
+  status_check
+
+  print_head "install nodejs dependancies"
+  cd /app &>>${LOG}
+  npm install &>>${LOG}
+  status_check
+
+  print_head "configure ${component} service file"
+  cp ${script_location}/files/${component}.service /etc/systemd/system/${component}.service &>>${LOG}
+  status_check
+
+  print_head "reload systemd"
+  systemctl daemon-reload &>>${LOG}
+  status_check
+
+  print_head "enable ${component} server"
+  systemctl enable ${component} &>>${LOG}
+  status_check
+
+  print_head "start ${component} server"
+  systemctl start ${component} &>>${LOG}
+  status_check
+
+  print_head "configuring mongo repo"
+  cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
+  status_check
+
+  print_head "install mongo client"
+  yum install mongodb-org-shell -y &>>${LOG}
+  status_check
+
+  print_head "load schema"
+  mongo --host mongodb-dev.anandnandhu.online </app/schema/${component}.js &>>${LOG}
+  status_check
+}
---
+++ user.sh
@@ -0,0 +1,5 @@
+source commom.sh
+
+component=user
+
+NODEJS
